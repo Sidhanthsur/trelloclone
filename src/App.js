04e14f99bacd6ec1d01a7a2b4645d475e9fb8e47
developer <developer@192.168.0.101>
2019-06-14 20:16:28 +0530

    safe

--- src/App.js
+++ src/App.js
@@ -2,34 +2,53 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Board from './Resources/react-trello-master/src'
-
-const data = {
-  lanes: [
-    {
-      id: 'lane1',
-      title: 'Planned Tasks',
-      label: '2/2',
-      cards: [
-        {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
-        {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
-      ]
-    },
-    {
-      id: 'lane2',
-      title: 'Completed',
-      label: '0/0',
-      cards: []
-    }
-  ]
+import PouchDB from 'pouchdb-browser'
+var db = new PouchDB('zoomrx');
+const initialData = {
+  _id: 'personal',
+  lanes: []
 }
+
+
+
 
 var onclickof = () => {
   
 }
+
 function App() {
 
+  const [data, setData] = useState(initialData)
+  var initializeDb = () => {
+    db.put(initialData)
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+  }
+  
+  db.get('personal')
+  .then((doc) => {
+    setData(doc)
+  })
+  .catch((error) => {
+    initializeDb()
+  })
+  var onLaneAdded = async (params) => {
+    let localData = data
+    localData.lanes.push({id: params.title, title: params.title, cards: []})
+    setData(localData)
+    try {
+      let doc = await db.get('personal')
+      if (doc) {
+        let localObject = Object.assign({}, localData, {_rev: doc._rev})
+        let response = await db.put(localObject)
+        console.log(response)
+      }
+    }
+    catch (error) {
+      console.error(error)
+    }
 
-
+  }
   return (
     <div className="App">
      
@@ -37,7 +56,8 @@ function App() {
       draggable
       canAddLanes
       editable
-      onCardAdd={() => console.log('card added')}
+      onLaneAdd={onLaneAdded}
+      onCardAdd={(card, laneid) => console.log(laneid)}
       data={data} />
     </div>
   );
