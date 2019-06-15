7f45a59d8bf29e2c77cdde0724f7e4ddf3140e60
sidhanth <sidhanth@hatchsquad.com>
2019-06-15 14:43:38 +0530

    Updates

---
+++ precache-manifest.3cc021a75c84f082949721220a48770f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421157855363e4318bd1d7933c74ca7f",
+    "url": "/trelloclone/index.html"
+  },
+  {
+    "revision": "3ca9d90c3e62dbed5dd4",
+    "url": "/trelloclone/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b2e1ac33416a614a35df",
+    "url": "/trelloclone/static/js/2.22c38776.chunk.js"
+  },
+  {
+    "revision": "3ca9d90c3e62dbed5dd4",
+    "url": "/trelloclone/static/js/main.23ee8fc6.chunk.js"
+  },
+  {
+    "revision": "ba250562d8e30013e450",
+    "url": "/trelloclone/static/js/runtime~main.7b28d136.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trelloclone/static/media/logo.5d5d9eef.svg"
+  }
+]);
